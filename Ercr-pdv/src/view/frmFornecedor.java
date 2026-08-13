66ccc5cc2b3655e7f9edd5cf07b20b81994f7573
Juan Rozas Santos <juanbr.rozas2005@gmail.com>
2026-08-12 22:30:12 -0300

    22:29

--- Ercr-pdv/src/view/frmFornecedor.java
+++ Ercr-pdv/src/view/frmFornecedor.java
@@ -13,6 +13,7 @@ public class frmFornecedor extends JDialog {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -41,30 +42,30 @@ public class frmFornecedor extends JDialog {
 		getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Nome");
-		lblNewLabel.setBounds(55, 62, 46, 14);
+		lblNewLabel.setBounds(52, 122, 46, 14);
 		getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Fone");
-		lblNewLabel_1.setBounds(55, 123, 46, 14);
+		lblNewLabel_1.setBounds(52, 183, 46, 14);
 		getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("E-mail");
-		lblNewLabel_2.setBounds(55, 185, 46, 14);
+		lblNewLabel_2.setBounds(52, 245, 46, 14);
 		getContentPane().add(lblNewLabel_2);
 		
 		textField = new JTextField();
-		textField.setBounds(103, 59, 483, 20);
+		textField.setBounds(100, 119, 368, 20);
 		getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(106, 120, 239, 20);
+		textField_1.setBounds(103, 180, 239, 20);
 		getContentPane().add(textField_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(103, 182, 483, 20);
+		textField_2.setBounds(100, 242, 483, 20);
 		getContentPane().add(textField_2);
 		
 		JButton btnNewButton = new JButton("Adicionar");
@@ -79,9 +80,22 @@ public class frmFornecedor extends JDialog {
 		btnExcluir.setBounds(369, 358, 89, 23);
 		getContentPane().add(btnExcluir);
 		
-		JButton btnNewButton_3 = new JButton("New button");
-		btnNewButton_3.setBounds(497, 358, 89, 23);
-		getContentPane().add(btnNewButton_3);
+		JButton btnRelario = new JButton("Relatório");
+		btnRelario.setBounds(494, 358, 89, 23);
+		getContentPane().add(btnRelario);
+		
+		JLabel lblNewLabel_3 = new JLabel("ID");
+		lblNewLabel_3.setBounds(52, 58, 46, 14);
+		getContentPane().add(lblNewLabel_3);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(100, 55, 92, 20);
+		getContentPane().add(textField_3);
+		
+		JButton btnNewButton_1 = new JButton("Buscar");
+		btnNewButton_1.setBounds(494, 118, 89, 23);
+		getContentPane().add(btnNewButton_1);
 		
 		// iniciar centralizado
 		setLocationRelativeTo(null);
